b89a6e0efcc3b29772ed55810894f32d2d315f82
Sadiq Hassan <abubakarhassan59@gmail.com>
2018-08-19 13:09:38 +0100

    fix: ARIA breadcrumb and image
    Improving Breadcrumb to follow WAI-ARIA 1.1 Design Pattern (nav contained).
    Add alternative text of images in homepage.
    Improved responsive design on small display size.
    Minor fix to support relative path.
    
    Resolves: N/A
    See also: N/A

--- js/main.js
+++ js/main.js
@@ -169,6 +169,7 @@ createRestaurantHTML = (restaurant) => {
   const image = document.createElement('img');
   image.className = 'restaurant-img';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
+  image.setAttribute('alt', 'Image of the ' + restaurant.name + ' restaurant');
   li.append(image);
 
   const name = document.createElement('h2');
