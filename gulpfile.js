cbfa219d43887833b40d8cd67fa7971130c31961
Sadiq Hassan <abubakarhassan59@gmail.com>
2018-10-22 06:23:50 +0100

    feat: improved JS with fetch API
    Support download of restaurant using XHR using fetch API.
    
    Resolves: N/A
    See also: N/A

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
 /*eslint-env node */
+
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
@@ -8,15 +9,18 @@ var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var babel = require('gulp-babel');
 var sourcemaps = require('gulp-sourcemaps');
+
 gulp.task('default', ['copy-html', 'copy-images', 'styles', 'lint', 'scripts'], function () {
   gulp.watch('sass/**/*.scss', ['styles']);
   gulp.watch('js/**/*.js', ['lint']);
-  gulp.watch('/index.html', ['copy-html']);
-  gulp.watch('./dist/index.html').on('change', browserSync.reload);
+  gulp.watch('/*.html', ['copy-html']);
+  gulp.watch('./dist/*.html').on('change', browserSync.reload);
+
   browserSync.init({
     server: './dist'
   });
 });
+
 gulp.task('dist', [
   'copy-html',
   'copy-images',
@@ -24,24 +28,35 @@ gulp.task('dist', [
   'lint',
   'scripts-dist'
 ]);
+
 gulp.task('scripts', function () {
   gulp.src('js/**/*.js')
     .pipe(babel())
     .pipe(gulp.dest('dist/js'));
+  gulp.src('js/*.js')
+    .pipe(babel())
+    .pipe(gulp.dest('dist/'));
 });
+
 gulp.task('scripts-dist', function () {
   gulp.src('js/**/*.js')
     .pipe(babel())
     .pipe(gulp.dest('dist/js'));
+  gulp.src('js/*.js')
+    .pipe(babel())
+    .pipe(gulp.dest('dist/'));
 });
+
 gulp.task('copy-html', function () {
   gulp.src('./*.html')
     .pipe(gulp.dest('./dist'));
 });
+
 gulp.task('copy-images', function () {
   gulp.src('img/*')
     .pipe(gulp.dest('dist/img'));
 });
+
 gulp.task('styles', function () {
   gulp.src('sass/**/*.scss')
     .pipe(sourcemaps.init())
@@ -55,10 +70,12 @@ gulp.task('styles', function () {
     .pipe(gulp.dest('dist/css'))
     .pipe(browserSync.stream());
 });
+
 gulp.task('lint', function () {
   return gulp.src(['js/**/*.js'])
     .pipe(eslint())
     .pipe(eslint.format())
     .pipe(eslint.failOnError());
 });
+
 gulp.task('tests', function () {});
